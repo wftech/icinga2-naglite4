a378be677c16f90101fb74b34e962f1dc5e1c342
Veros Kaplan <VerosK@users.noreply.github.com>
2018-12-29 18:10:06 +0100

    Load all hosts to local cache

--- models.py
+++ models.py
@@ -1,35 +1,47 @@
 import json
 import datetime
+from collections import OrderedDict
 from pprint import pprint
 
 from redis import StrictRedis
 
 
-
 class MonitoringStatus:
     def __init__(self, apiclient):
         """
-        Host representing monitoring status
+        This objects Representing monitoring status.
 
-        :param storage: Redis storage used for retrieving data
+        :param apiclient: Provide Icinga2API client
         """
 
         self.apiclient = apiclient
+        self._hosts_cache = None
+
+    def _hosts(self):
+
+        # TODO: this should probably use some thread lock and/or cache.
+        if self._hosts_cache is not None:
+            return self._hosts_cache
+
+        hosts = {}
+        for obj in self.apiclient.objects.list('Host'):
+            k = obj['attrs']['__name']
+            hosts[k] = Host(obj)
+
+        self._hosts_cache = OrderedDict(sorted(hosts.items()))
+        return self._hosts_cache
+
+    def all_hosts(self):
+        return self._hosts().values()
 
     def problem_hosts(self, acknowledged=None):
-        api = self.apiclient
-        filters = 'host.state!=ServiceOK'
-        retv = {}
-        for obj in api.objects.list('Host', filters=filters):
+        for obj in self._hosts().values():
+            if int(obj['state']) == 0:  # OK
+                continue
             if acknowledged is not None:
-                if int(obj['attrs']['acknowledgement']) != int(acknowledged):
+                if int(obj['acknowledgement']) != int(acknowledged):
                     continue
-            n = obj['attrs']['__name']
-            retv[n] = Host(obj)
-
-        for k,v in sorted(retv.items()):
-            yield v
-
+            yield obj
 
     @property
     def problem_services(self, acknowledged=None):
@@ -42,7 +54,7 @@ class MonitoringStatus:
                     continue
             n = obj['attrs']['__name']
             retv[n] = Service(obj)
-        for k,v in sorted(retv.items()):
+        for k, v in sorted(retv.items()):
             yield v
 
 
@@ -75,28 +87,32 @@ class Status:
         duration = datetime.datetime.now() - event_time
         return duration
 
+    def __getitem__(self, item):
+        return self._data['attrs'][item]
+
 
 class Host(Status):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         assert self._data['type'] == 'Host', self._data
-#        pprint(self._data)
+
+    #        pprint(self._data)
 
     @property
     def host_name(self):
-        return self._data['attrs']['name']
+        return self['name']
 
     @property
     def host_address(self):
-        return self._data['attrs']['address']
+        return self['address']
 
 
 class Service(Status):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         assert self._data['type'] == 'Service', self._data
-#        pprint(self._data)
 
+    #        pprint(self._data)
 
     @property
     def service_name(self):
